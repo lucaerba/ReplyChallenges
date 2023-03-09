56fe6f35e2f8f43c47c24b9e731b8f3243d91c59
LUCA ERBI s319024 <s319024@studenti.polito.it>
2023-03-09 18:19:54 +0100

    output ok

--- 2023/main.cpp
+++ 2023/main.cpp
@@ -1,5 +1,5 @@
 #include "libraries.h"
-
+#define INPUT "./2023/00-example.txt"
 using namespace std;
 void printOut();
 
@@ -7,6 +7,8 @@ class Point{
 public: int x, y;
 public:
     Point(int x, int y) : x(x), y(y) {}
+
+    Point() {}
 };
 class Warmhole: public Point{
 public: int id, max_id, max_pt;
@@ -19,12 +21,12 @@ public:
     Valuable(int x, int y, int id, int value) : Point(x, y), id(id), value(value) {}
 };
 class Snake{
-public: int id;
+public: int id, l;
 public: Point start;
 public: string path;
 
 public:
-    Snake(int id, string path) : id(id), path(path), start(Point(-1,-1)) {}
+    Snake(int id, string path, int l) : id(id), path(path), start(Point(-1,-1)), l(l) {}
 };
 
 int C, R, S;
@@ -35,31 +37,37 @@ vector<Warmhole> wormholes;
 Point** matr;
 
 
-
-};
-
 void findSolution();
 void optimizeSolution();
-
+void readInput();
+bool compareValue(Valuable v1, Valuable v2){
+    return v1.value > v2.value;
+}
 int main(const int argc, const char * argv[]) {
-    cout << "prova";
+    readInput();
+    std::sort(relevance.begin(), relevance.end(), compareValue);
+    findSolution();
+    optimizeSolution();
+    printOut();
     return 0;
 }
 
 void readInput() {
     ifstream file;
-    file.open("input.txt");
+    file.open(INPUT);
     file >> C >> R >> S;
-    matr = new Point*[C*R];
+    matr = new Point*[R];
+    for(int i=0; i<R; i++) matr[i] = new Point[C];
+
     for (int i = 0; i < S; i++) {
         int temp;
         file >> temp;
-        snakes.push_back(Snake(i, ""));
+        snakes.push_back(Snake(i, "", temp));
     }
     int count = 0;
     for (int i = 0; i < C; i++) {
         for (int j = 0; j < R; j++) {
-            int temp;
+            char temp;
             file >> temp;
            
             if (temp == '*') {
@@ -67,7 +75,7 @@ void readInput() {
                 wormholes.push_back(wh);
                 matr[i*C+j] = &wh;
             } else {
-                Valuable r = Valuable(i, j, count++, temp);
+                Valuable r = Valuable(i, j, count++, temp-48);
                 relevance.push_back(r);
                 matr[i*C+j] = &r;
             }
@@ -76,7 +84,18 @@ void readInput() {
     file.close();
 }
 void findSolution(){
-
+    for(auto s: snakes){
+        Valuable v = relevance.at(0);
+        relevance.erase(relevance.begin());
+        int l = s.l-1;
+        while(l > 0){
+            if(v.y<R-1)
+                matr[v.x+1+C*v.y];
+            if(v.x<C-1)
+                matr[v.x+C*(v.y+1)];
+            if()
+        }
+    }
 }
 void optimizeSolution(){
 
